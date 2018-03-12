2aaa77395fbc93a86e287a57a8bf8448812897ff
Unknown <gsinghmakkar@hotmail.co.uk>
2018-03-12 23:25:57 +0000

    Store currency in session

--- src/main/java/com/envoydigital/controllers/ProductListController.java
+++ src/main/java/com/envoydigital/controllers/ProductListController.java
@@ -1,9 +1,12 @@
 package com.envoydigital.controllers;
 
 import com.envoydigital.service.ProductService;
+import com.sun.xml.internal.ws.util.StringUtils;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -15,8 +18,9 @@ public class ProductListController {
 	private ProductService productService;
 
 	@RequestMapping("/product/list")
-	public String getProductListPage(Model model) {
+	public String getProductListPage(Model model, HttpSession session) {
 		model.addAttribute("products", productService.getProducts());
+		session.setAttribute("currency", "GBP");
 		return "productList";
 	}
 
